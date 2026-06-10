6709f536b85193c1eb1b388d11035ee836a0f73d
Henrique Lopes <henriquels.dev@gmail.com>
2026-06-10 13:34:12 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
@@ -25,6 +28,30 @@ const Home = () => {
             className="rounded-xl object-cover"
             alt="Agende nos melhores com fsw "
           />
+        </div>
+        <div>
+          <Card className="mt-6">
+            <CardContent className="flex justify-between p-0">
+              {/* Esquerda */}
+              <div className="flex flex-col gap-2 py-5 pl-5">
+                <Badge>Confirmado</Badge>
+                <h3 className="font-bold">Corte de cabelo</h3>
+
+                <div className="flex items-center gap-2">
+                  <Avatar className="h-6 w-6">
+                    <AvatarImage src="https://utfs.io/f/988646ea-dcb6-4f47-8a03-8d4586b7bc21-16v.png" />
+                  </Avatar>
+                  <p className="text-sm">Vintage Barber</p>
+                </div>
+              </div>
+              {/* Direita */}
+              <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+                <p className="font-sm">Fevereiro</p>
+                <p className="font-bold">06</p>
+                <p className="font-sm">09:45</p>
+              </div>
+            </CardContent>
+          </Card>
         </div>
       </div>
     </div>
